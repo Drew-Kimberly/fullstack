0bbf40f78a98445aabd44213d20a83153423d073
Andrew Kimberly <andkim@i-6e0742e7.us-east-1b.nadq.io>
2016-04-13 22:02:38 +0000

    Server-side file upload implementation

--- vagrant/catalog/app.py
+++ vagrant/catalog/app.py
@@ -1,14 +1,21 @@
+import os
 from flask import Flask, render_template, url_for, request, jsonify
+from werkzeug.utils import secure_filename
 from AjaxHandler import AjaxHandler
+from ImageHelper import ImageHelper
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, Item
 import json
 
-app = Flask(__name__)
+UPLOAD_FOLDER = '/static/img'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
-engine = create_engine('postgresql:///catalog')
-# engine = create_engine('mysql://andkim:andkim@localhost:3306/catalog', pool_recycle=3600)
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# engine = create_engine('postgresql:///catalog')
+engine = create_engine('mysql://andkim:andkim@localhost:3306/catalog', pool_recycle=3600)
 Base.metadata.bind = engine
 
 DBSessionMaker = sessionmaker(bind=engine)
@@ -32,11 +39,19 @@ def catalog():
             else:
                 return False
         else:
-            # Handle uploaded files in ajax post
-
+            #File included in request - first save the file in filesystem
+            image_file = request.files['file_data']
+            if image_file and is_image(image_file.filename):
+                image_filename = secure_filename(image_file.filename)
+                image_file.save(os.path.join(app.config['UPLOAD_FOLDER'], image_filename))
             post_data = json.loads(request.form['request_data'])
             ajax_handler.posted_data = post_data
             response_data = ajax_handler.processRequest()
+            # Handle uploaded files in ajax post
+            image_helper = ImageHelper(request.files['file_data'])
+            if image_helper.save_file():
+
+
             if response_data:
                 return process_response(response_data)
             else:
@@ -56,6 +71,8 @@ def process_response(response_data):
 
     return json.dumps(response)
 
+def is_image(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 if __name__ == "__main__":
     app.debug = True
