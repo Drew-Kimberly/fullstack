0bfe14993bd32a9ffd006188b0a1c6ab1f446103
Drew Kimberly <andrew.e.kimberly@gmail.com>
2016-04-19 21:48:04 -0400

    Course 4 work - beginnings

--- scalable_apps/Lesson_2/Hello_Endpoints/helloworld_api.py
+++ scalable_apps/Lesson_2/Hello_Endpoints/helloworld_api.py
@@ -16,6 +16,12 @@ from protorpc import remote
 REQUEST_CONTAINER = endpoints.ResourceContainer(
     message_types.VoidMessage,
     name=messages.StringField(1),
+    period=messages.StringField(2)
+)
+
+REQUEST_GREETING_CONTAINER = endpoints.ResourceContainer(
+    name=messages.StringField(1),
+    period=messages.StringField(2),
 )
 
 
@@ -41,6 +47,12 @@ class HelloWorldApi(remote.Service):
     def say_hello_by_name(self, request):
       greet = "Hello {}".format(request.name)
       return Hello(greeting=greet)
+
+    @endpoints.method(REQUEST_GREETING_CONTAINER, Hello,
+      path="greetByPeriod", http_method='GET', name="greetByPeriod")
+    def greet_by_period(self, request):
+        greet = "Good {0}, {1}".format(request.period, request.name)
+        return Hello(greeting=greet)
 
 
 APPLICATION = endpoints.api_server([HelloWorldApi])
